4f2cdc83ad3519ad56411c884303a863e87f2d62
MalkingMan <arrayalkhozini@gmail.com>
2025-11-27 12:02:57 +0700

    Add SiswaApp composable entry point
    Introduce the `SiswaApp` composable function in `PetaNavigasi.kt` to serve as the main entry point, initializing the navigation controller and calling `HostNavigasi`.

--- app/src/main/java/com/example/roomdatabase/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/roomdatabase/view/uicontroller/PetaNavigasi.kt
@@ -1,2 +1,14 @@
 package com.example.roomdatabase.view.uicontroller
 
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun SiswaApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier = Modifier
+) {
+    HostNavigasi(navController = navController, modifier = modifier)
+}
